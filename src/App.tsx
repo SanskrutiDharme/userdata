44fb430b0e15914c68b50779bb11f54f16a91ce1
Alex@0123 <sanskrutidharme@outlook.com>
2023-06-01 19:04:05 +0530

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  
+  const userAPI = () => {
+    return new Promise<any>((resolve ,reject) => {
+      return setTimeout(() => {
+        resolve({name: 'Sanskruti'});
+      }, 1000);
+   })
+ };
+
+  const nonAsyn = () => {
+    setTimeout(() => {
+       console.log('from non asyn')
+    },1000);
+  }
+ 
+ const getUserData = async () => {
+  //const userData = await userAPI();
+  //console.log('userData' ,userData)
+
+  console.log('test before')
+  nonAsyn()
+  console.log('test after')
+ }
+
+  const [title , setTitle ] = useState('not change');
+  useEffect( () => {
+    getUserData()
+    
+  }, [])
+
+
+return (
+  <div>
+    use effect {title}
+  </div>
+
+ );
+
+}
+
+export default App;
